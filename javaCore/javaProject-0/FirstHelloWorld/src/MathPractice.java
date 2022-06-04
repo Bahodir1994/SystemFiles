245acdc2787312e3b05d463f96cecd4526bc0e0a
Imomuddinov Bahodir <imomuddinov@inbox.ru>
2022-06-04 16:42:28 +0500

    Math Project

---
+++ javaCore/javaProject-0/FirstHelloWorld/src/MathPractice.java
@@ -0,0 +1,13 @@
+public class MathPractice {
+    public static void main(String[] args){
+        String name = "Husniddin";
+        String className= "Java Backend";
+        int age = 32;
+        int position = 5;
+
+        System.out.println("Name: "+ name + " " + "className" + className);
+        System.out.println("Name: "+ name + "my age: " + "myPosition: " + position);
+
+
+    }
+}
