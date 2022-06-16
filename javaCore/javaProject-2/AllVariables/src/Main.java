bf1bea816d105020e38b0c124a4a200b2ccae802
Imomuddinov Bahodir <imomuddinov@inbox.ru>
2022-06-16 19:17:19 +0500

    java 3

---
+++ javaCore/javaProject-2/AllVariables/src/Main.java
@@ -0,0 +1,16 @@
+import java.math.BigDecimal;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+
+
+
+    /*
+    * Uyga vazifa
+    * 1..10 gacha shu sonlarni juftini , toqini, musbatini, mafiyini topish
+    * */
+
+}
